c3cf555ccb4ab7c69b6a9b695f5ac9d47e153db5
arthur-il-de-souza <arthurinaciocg@gmail.com>
2023-02-20 16:02:30 -0300

    Bootstrap, Jquery e components criados/anexados no projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopComponent } from './views/top/top.component';
 import { CarouselComponent } from './views/carousel/carousel.component';
+import { NavComponent } from './views/nav/nav.component';
+import { NavBotComponent } from './views/nav-bot/nav-bot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopComponent,
     CarouselComponent,
+    NavComponent,
+    NavBotComponent,
   ],
   imports: [
     BrowserModule,

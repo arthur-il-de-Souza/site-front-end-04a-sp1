91eab6cf9c27e9cbc30d7eea4cb8ef735a10d4cf
arthur-il-de-souza <arthurinaciocg@gmail.com>
2023-02-23 16:49:29 -0300

    Add product cards

---
+++ src/app/views/main-cards/main-cards.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-cards',
+  templateUrl: './main-cards.component.html',
+  styleUrls: ['./main-cards.component.css']
+})
+export class MainCardsComponent {
+
+}
